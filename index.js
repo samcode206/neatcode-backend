43f6b40710bd0608f35e73221c75f59ae7d740d7
samcode206 <66322745+samcode206@users.noreply.github.com>
2020-12-31 23:44:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express'); 
 const app = express(); 
 app.use(express.json());
+const cors = require('cors')
+server.use(cors())
 const questionsRouter = require('./routes/questions.js');
 const entryRouter = require('./routes/auth/entry.js');   
 const todoRouter = require('./routes/user/todo/todoRouter.js');
@@ -9,6 +11,7 @@ const fulfilledRouter = require('./routes/user/fulfilled/fulfilledRouter.js');
 const updateQuestionsPerDay = require('./routes/user/preferance/updateQuestionsPerDay.js');
 const notesRouter = require('./routes/user/notes/notesRouter.js');
 const jobsRouter = require('./routes/user/jobs/jobsRouter.js'); 
+
 job;
 
 app.get('/' , (req,res)=>{
