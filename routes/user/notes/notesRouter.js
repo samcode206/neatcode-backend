cd5d4c23d3cea91ffd12089ea9c67ab067d9d796
samcode206 <66322745+samcode206@users.noreply.github.com>
2021-01-04 14:58:53 -0800

    Update notesRouter.js

--- routes/user/notes/notesRouter.js
+++ routes/user/notes/notesRouter.js
@@ -4,16 +4,22 @@ router = express.Router(),
 authorize = require('../../auth/authUtils/auth-mid.js'),
 addNote = require('./addNote.js'),
 removeNote = require('./removeNote.js'),
-getNote = require('./addNote.js');
-
+ {User} = require('../../../db.js');
 // get note 
-router.get('/getnote', authorize, async (req, res)=>{
-    getNote(req,res); 
+router.get('/', authorize, async (req, res)=>{
+      try{
+    const username = req.header('username');
+    const user = await User.findOne().where("username").equals(username);
+    res.status(200).json(user.notes);
+    }catch(err){
+        res.status(500).json({message: err.message}); 
+    };
 });
 
 
 // add a note
 router.post('/', authorize, async (req, res) =>{
+
    addNote(req,res);
 });
 
