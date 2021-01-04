244182462e90146919c93471eb71e3b6d39cf3dd
samcode206 <66322745+samcode206@users.noreply.github.com>
2021-01-03 21:36:28 -0800

    Update notesRouter.js

--- routes/user/notes/notesRouter.js
+++ routes/user/notes/notesRouter.js
@@ -3,7 +3,14 @@ express = require('express'),
 router = express.Router(),
 authorize = require('../../auth/authUtils/auth-mid.js'),
 addNote = require('./addNote.js'),
-removeNote = require('./removeNote.js');
+removeNote = require('./removeNote.js'),
+getNote = require('./addNote.js');
+
+// get note 
+router.get('/', authorize, async (req, res)=>{
+    getNote(req,res); 
+});
+
 
 // add a note
 router.post('/', authorize, async (req, res) =>{
